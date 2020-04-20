1d42c4b64b30569f34f744c81601988da54b63fd
Randy Koziol <randy312k@gmail.com>
2020-04-19 20:34:22 -0400

    Commited

---
+++ src/main/java/com/finalproject/validator/DateValidator.java
@@ -0,0 +1,30 @@
+package com.finalproject.validator;
+
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class DateValidator {
+
+    @FacesValidator
+    public class ItemValidator implements Validator {
+        private String regex = "^([0-2][0-9]|(3)[0-1])(/)(((0)[0-9])|((1)[0-2]))(/)\\d{4}$";
+
+
+        @Override
+        public void validate(FacesContext facesContext, UIComponent uiComponent, Object name) {
+            if (!Pattern.compile(regex).matcher(name.toString()).matches()) {
+                FacesMessage msg =
+                        new FacesMessage("Date format is dd/mm/yyyy");
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(msg);
+            }
+        }
+    }
+}
